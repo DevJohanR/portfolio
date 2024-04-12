3a4ade0b9f18a94dec5bb0e0e9d005a31463fb7c
DevJohanR <desarrollador7777@gmail.com>
2024-04-12 18:49:43 -0500

    primer backup de mi portafolio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Navbar from './components/Navbar';
+import Hero from './components/HeroSection';
+import Skills from './components/Skills';
+import Education from './components/Education';
+
+function App() {
+
+
+  return (
+    <>
+  <Navbar/>
+  <Hero/>
+  <Skills/>
+  <Education/>
+    </>
+  )
+}
+
+export default App
